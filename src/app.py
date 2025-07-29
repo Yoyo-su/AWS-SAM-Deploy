5f0a01b70a6f29363acd2a7f532e24e467d75688
Yoyo-su <iohannan211@gmail.com>
2025-07-29 14:58:15 +0100

    update function as API Gateway response

--- src/app.py
+++ src/app.py
@@ -1,2 +1,20 @@
+import json
+
+def respond(err, res=None):
+    if err:
+        return {
+            "statusCode": 400,
+            "body": f"Error: {err.message}"
+        }
+    else:
+        return {
+            "statusCode": 400 if err else 200,
+            "body": err.message if err else  json.dumps(res),
+            "headers": {
+                "Content-Type": "application/json"
+            }            
+        }
+
 def lambda_handler(event, context):
-    return "Hello, World!"
+    print("Received event: " + json.dumps(event, indent=2))
+    return respond(None, res="Hello, World!")
